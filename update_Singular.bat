23597456ce8bf9b7ab7300ab62343491fc1ae7a9
Ray <lanskyassassination@gmail.com>
2024-11-27 13:40:36 +0800

    更新 update_Singular.bat 腳本以備份舊的 Singular 資料夾並清除其內容，確保下載最新版本時不會遺失資料

--- update_Singular.bat
+++ update_Singular.bat
@@ -4,18 +4,25 @@ setlocal
 rem Set variables
 set "DOWNLOAD_URL=https://github.com/Shen-Ming-Hong/singular_ardublockly/archive/refs/heads/main.zip"
 set "TARGET_DIR=%~dp0Singular"
+set "BAK_DIR=%~dp0Singular_bak"
 set "TEMP_ZIP=%TEMP%\main.zip"
 set "EXTRACT_DIR=%TEMP%\extracted"
 
-rem Create the Singular folder if it doesn't exist
-if not exist "%TARGET_DIR%" (
-    mkdir "%TARGET_DIR%"
+rem Remove old backup folder if it exists
+echo Removing old backup folder if it exists...
+if exist "%BAK_DIR%" (
+    rd /s /q "%BAK_DIR%"
 )
 
-rem Clear the contents of the Singular folder
-echo Clearing the Singular folder...
-del /q "%TARGET_DIR%\*.*"
-for /d %%D in ("%TARGET_DIR%\*") do rd /s /q "%%D"
+rem Rename the current Singular folder to Singular_bak
+if exist "%TARGET_DIR%" (
+    echo Backing up current Singular folder...
+    rename "%TARGET_DIR%" Singular_bak
+)
+
+rem Create the Singular folder
+echo Creating new Singular folder...
+mkdir "%TARGET_DIR%"
 
 rem Use PowerShell to download the latest version of the ZIP file
 echo Downloading the latest version of the ZIP file...
@@ -40,7 +47,7 @@ if %ERRORLEVEL% neq 0 (
 rem Move the contents of the extracted folder to the target folder
 echo Moving extracted files to the target folder...
 for /d %%D in ("%EXTRACT_DIR%\*") do (
-    move "%%D\*" "%TARGET_DIR%\"
+    move "%%D\*" "%TARGET_DIR%\" >nul
 )
 
 rem Clean up temporary files
